233c3c70a89119fda06e36264fbe3fc48e59b639
Harry <harryrock67@gmail.com>
2019-02-14 15:55:59 -0500

    added all vertexes on face reactive to ml5 and audio, also connected the dots to each other

--- js/ParticleSystem.js
+++ js/ParticleSystem.js
@@ -1,18 +1,30 @@
 class ParticleSystem {
     constructor(points) {
         this.points = points;
-        this.particles = [];
-        this.particles[0] = new Particle(width/2, height/2);
+        this.poseParticles = [];
+        for (let i = 0; i < 6; i++) {
+            this.poseParticles[i] = new PoseParticle(width / 2, height / 2);
+        }
     }
     show() {
-        const pos = createVector(this.getPointPos(0, 'x'), this.getPointPos(0, 'y'))
-        this.particles[0].show(pos);
-    }
-    getPointPos (at, which) {
         if (points != undefined && points.length > 0) {
-            return which == 'x' ? width - points[at].pose.keypoints[at].position.x : points[at].pose.keypoints[at].position.y;
-        } else {
-            return false
+            for (let i = 0; i < this.poseParticles.length - 1; i++) {
+                for (let j = 0; j < this.poseParticles.length - 1; j++) {
+                    this.poseParticles[i].connectTo(this.poseParticles[j]);
+                }
+                const pos = createVector(this.getPointPos(i, 'x'), this.getPointPos(i, 'y'))
+                const z = dist(this.getPointPos(1, 'x'), this.getPointPos(1, 'y'), this.getPointPos(2, 'x'), this.getPointPos(2, 'y'));
+                this.poseParticles[i].show(pos, z);
+            }
+            this.drawLipParticle(this.getPointPos(1, 'x'), this.getPointPos(1, 'y'), this.getPointPos(2, 'x'), this.getPointPos(2, 'y'));
         }
+    }
+    drawLipParticle(x1, y1, x2, y2) {
+        const pos = createVector((x1 - x2) + x1, y1 + (y1 - y2) + gap);
+        const gap = dist(x1, y1, x2, y2);
+        this.poseParticles[5].show(pos, gap);
+    }
+    getPointPos(at, which) {
+        return which == 'x' ? width - points[0].pose.keypoints[at].position.x : points[0].pose.keypoints[at].position.y;
     }
 }
--- js/PoseParticle.js
+++ js/PoseParticle.js
@@ -1,27 +1,30 @@
-class Particle {
+class PoseParticle {
     constructor(x, y) {
         this.x = x;
         this.y = y;
         this.children = [];
         this.childrenAngle = 0;
-        for(let i = 0; i < 5; i++) {
-            this.children = new ParticleChildren(i, 5);
+        for(let i = 0; i < 10; i++) {
+            this.children[i] = new ParticleChildren(i, 10);
         }
     }
-    show(pos){
+    show(pos, z){
         this.update();
-        const {x,y} = this.move(pos)
-        ellipse(x, y, 20, 20);
+        const mapZ = map(z, 0, width, 0, 80);
+        const {x, y} = this.move(pos)
+        ellipse(x, y, mapZ + mp3.vol * 100, mapZ + mp3.vol * 100);
         this.drawChilden(x, y);
     }
     update() {
-        this.childrenAngle += 0.01;
+        this.childrenAngle += 0.08;
         noStroke();
         fill(255);
     }
     move(pos) {
         const newX = lerp(pos.x, this.x, .5);
         const newY = lerp(pos.y, this.y, .5);
+        this.x = newX;
+        this.y = newY;
         return {x: newX,y: newY}
     }
     drawChilden(x, y) {
@@ -29,6 +32,11 @@ class Particle {
             this.children[i].updatePos(x, y, this.childrenAngle);
             this.children[i].show();
         }
+    }
+    connectTo(other) {
+        noFill();
+        stroke(50);
+        line(this.x, this.y, other.x, other.y);
     }
 }
 
@@ -38,15 +46,17 @@ class ParticleChildren {
         this.siblingLength = siblingLength;
         this.x = 0;
         this.y = 0;
-        this.r = 40;
+        this.rOff = random(100);
+        this.r;
     }
     show() {
-        this.updatePos();
-        ellipse(this.x, this.y, 5, 5);
+        ellipse(this.x, this.y, 2, 2);
     }
     updatePos(x, y, childrenAngle) {
-        const relAngle = this.relIndex / this.siblingLength * 3.14;
+        this.r = map(noise(this.rOff), 0, 1, 10, 20)
+        const relAngle = this.relIndex / this.siblingLength * 6.28;
         this.x = this.r * Math.sin(childrenAngle + relAngle) + x;
-        this.y = this.r * Math.sin(childrenAngle + relAngle) + y;
+        this.y = this.r * Math.cos(childrenAngle + relAngle) + y;
+        this.rOff += 0.01;
     }
 }
