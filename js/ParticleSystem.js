85cda24b459ee46ee4151c2190a6dd6f4ddb2d55
Harry <harryrock67@gmail.com>
2019-02-14 16:07:44 -0500

    nose particle manually created

--- js/ParticleSystem.js
+++ js/ParticleSystem.js
@@ -20,9 +20,12 @@ class ParticleSystem {
         }
     }
     drawLipParticle(x1, y1, x2, y2) {
-        const pos = createVector((x1 - x2) + x1, y1 + (y1 - y2) + gap);
         const gap = dist(x1, y1, x2, y2);
+        const pos = createVector((x1 + x2) / 2 + (y1 - y2), (y1 + y2) / 2 + gap);
         this.poseParticles[5].show(pos, gap);
+        for (let j = 0; j < this.poseParticles.length - 1; j++) {
+            this.poseParticles[5].connectTo(this.poseParticles[j]);
+        }
     }
     getPointPos(at, which) {
         return which == 'x' ? width - points[0].pose.keypoints[at].position.x : points[0].pose.keypoints[at].position.y;
