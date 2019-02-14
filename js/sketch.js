2b93a8c373bf8acac81551aa6f12efbd2287faab
Harry <harryrock67@gmail.com>
2019-02-14 04:19:00 -0500

    check

--- js/sketch.js
+++ js/sketch.js
@@ -18,7 +18,11 @@ function draw() {
     background(0);
     mp3.updateVol();
     if (init) {
-        ellipse(posePos(0, 'x'), posePos(0, 'y'), 20, 20)
+        const newX = posePos(0, 'x');
+        const newY = posePos(0, 'y');
+        const x = lerp(x, newX, .5);
+        const y = lerp(y, newX, .5);
+        ellipse(x, y, 20, 20);
     }
 }
 
@@ -27,7 +31,6 @@ function windowResized() {
 }
 
 const loadAssets = () => song.isLoaded() ? (document.getElementById('play').classList.add('in'), mp3 = new MP3(song)) : 0;
-
 const initSketch = () => {
     init = true;
     song.play();
@@ -37,7 +40,6 @@ const initSketch = () => {
     pose = new Pose(video);
     pose.fetchPoints();
 }
-
 const posePos = (at, which) => {
     if (points != undefined && points.length > 0) {
         return which == 'x' ? width - points[at].pose.keypoints[at].position.x : points[at].pose.keypoints[at].position.y;
