9f0aca34ab9188bcc7a80ba47f21194860eebc7c
Harry <harryrock67@gmail.com>
2019-02-14 23:22:45 -0500

    skimmed version is functional

--- js/Pose.js
+++
@@ -1,7 +0,0 @@
-class Pose {
-    constructor(video) {
-        this.points;
-        this.pose = ml5.poseNet(video);
-        this.pose.on('pose', (poses) => points = poses);
-    }
-}
--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,13 @@
 class Sketch {
     constructor() {
         this.fetchVideo();
-        this.createPose();
+        ml5.poseNet(video).on('pose', (poses) => points = poses);
         this.particleSystem = new ParticleSystem(points);
     }
     init() {
         push();
         if (points != undefined && points.length > 0) {
-            this.getCanvasRotation();
-            rotateY(canvasRotationY);
+            this.getCnvRot();
             translate(-width / 4 - (mp3.vol * width / 20), -height / 4 - (mp3.vol * height / 20));
             scale(.5 + mp3.vol / 10);
             noFill();
@@ -22,17 +21,17 @@ class Sketch {
         video = createCapture(VIDEO);
         video.size(width, height);
     }
-    createPose() {
-        pose = new Pose(video);
-        //pose.fetchPoints();
-    }
-    getCanvasRotation() {
+    getCnvRot() {
+        let rot = {};
         const distLeft = dist(this.posePos(0, 'x'), this.posePos(0, 'y'), this.posePos(3, 'x'), this.posePos(3, 'y'));
         const distRight = dist(this.posePos(0, 'x'), this.posePos(0, 'y'), this.posePos(4, 'x'), this.posePos(4, 'y'));
         const maxDist = dist(this.posePos(4, 'x'), this.posePos(4, 'y'), this.posePos(3, 'x'), this.posePos(3, 'y')) / 2;
-        distLeft < distRight ? canvasRotationY = map(distLeft, 0, maxDist, -.2, 0) : canvasRotationY = map(distRight, 0, maxDist, .2, 0);
+        distLeft < distRight ? rot.y = map(distLeft, 0, maxDist, -.2, 0) : rot.y = map(distRight, 0, maxDist, .2, 0);
+        rotateY(this.cnvRotation.y);
+        rotateX(this.cnvRotation.x);
+        rotateZ(this.cnvRotation.Z);
     }
     posePos(at, which) {
-            return which == 'x' ? width - points[0].pose.keypoints[at].position.x : points[0].pose.keypoints[at].position.y;
+        return which == 'x' ? width - points[0].pose.keypoints[at].position.x : points[0].pose.keypoints[at].position.y;
     }
 }
