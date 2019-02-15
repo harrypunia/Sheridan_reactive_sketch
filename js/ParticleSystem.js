c0d9908bc1038a7f469d4385cbb83bea01d75790
Harry <harryrock67@gmail.com>
2019-02-14 19:34:07 -0500

    added rotation to canvas in respect to face detection

--- js/ParticleSystem.js
+++ js/ParticleSystem.js
@@ -1,11 +1,9 @@
 class ParticleSystem {
     constructor(points) {
         this.poseParticleSystem = new PoseParticleSystem(points);
-        this.sParticleSystem = new SParticleSystem(points);
         
     }
     show() {
         this.poseParticleSystem.draw();
-        this.sParticleSystem.show();
     }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -5,13 +5,14 @@ let song;
 let mp3;
 let points;
 let particleSystem;
+let canvasRotation = 0;
 
 function preload() {
     song = loadSound('assets/song.mp3');
 }
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(window.innerWidth, window.innerHeight, WEBGL);
     loadAssets();
 }
 
@@ -19,15 +20,25 @@ function draw() {
     background(0);
     mp3.updateVol();
     if (init) {
+        push();
+        if (points != undefined && points.length > 0) {
+            getCanvasRotation();
+            rotateY(canvasRotation);
+            rotateX(canvasRotation);
+            translate(-width/4, -height/4);
+            scale(.5);
+            noFill();
+            stroke(255);
+            rect(5, 5, width - 10, height - 10);
+        }
         particleSystem.show();
-        console.log(frameRate());
+        push();
     }
 }
 
 function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
-
 const loadAssets = () => song.isLoaded() ? (document.getElementById('play').classList.add('in'), mp3 = new MP3(song)) : 0;
 const initSketch = () => {
     init = true;
@@ -41,4 +52,16 @@ const declareObjects = () => {
     pose = new Pose(video);
     pose.fetchPoints();
     particleSystem = new ParticleSystem();
+}
+const getCanvasRotation = () => {
+    const distLeft = dist(posePos(0, 'x'), posePos(0, 'y'), posePos(3, 'x'), posePos(3, 'y'));
+    const distRight = dist(posePos(0, 'x'), posePos(0, 'y'), posePos(4, 'x'), posePos(4, 'y'));
+    const maxDist = dist(posePos(4, 'x'), posePos(4, 'y'), posePos(3, 'x'), posePos(3, 'y')) / 2;
+    let left;
+    let r;
+    distLeft < distRight ? (r = distLeft, left = true) : (r = distRight, left = false);
+    left ? canvasRotation = map(r, 0, maxDist, -.2, 0) : canvasRotation = map(r, 0, maxDist, .2, 0);
+}
+const posePos = (at, which) => {
+    return which == 'x' ? width - points[0].pose.keypoints[at].position.x : points[0].pose.keypoints[at].position.y;
 }
